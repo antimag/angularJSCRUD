2c6e5287594ca36ab7a5d3cbad455dc9cd8e77d9
antimag <antima@grepruby.com>
2014-10-30 19:22:22 +0530

    git add all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CrudWithAngularjs::Application.config.secret_key_base = 'f208d05c006667ceb7f0c5ea85295405cdbc54bc575526e09bcb02432943fc405c531e275e79fa76276a9eb6536081ecb38e1f03d329ec3418e37f24c1b114f4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CrudWithAngularjs::Application.config.session_store :cookie_store, key: '_crud_with_angularjs_session'
